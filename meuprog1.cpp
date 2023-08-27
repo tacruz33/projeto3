5421421636b132fd3f287f7b4f132e544cd29087
Tales Araujo <talesdearaujo18@gmail.com>
2023-08-26 21:32:25 -0300

    atividade

---
+++ meuprog1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+   
+    int main (void){
+
+        int nome_do_usuario
+        
+        cout << " Digite seu nome : ";
+        cin >> nome_do_usuario >>;
+         cout <<
+
+        
+
+
+
+
+     return 0;
+
+    }
