cd426ddeec634f0f94b0ea925ea517e9b21f0b05
tacruz33 <talesdearaujo18@gmail.com>
2023-08-28 17:51:53 -0300

    ss

---
+++ aula dia28.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main (void){
+    int d1,d2,q,r;
+    float x;
+ bool t;
+    d1 =5;
+    d2 =2;
+    t=d1==d2;
+ 
+    cout << t << endl;
+    return 0;
+}
