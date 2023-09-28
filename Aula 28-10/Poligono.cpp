ad6b4034014b7cf4583e33c8baa1f15a1490a1d9
Tales <talesdearaujo18@gmail.com>
2023-09-28 16:30:06 -0300

    A

---
+++ Aula 28-10/Poligono.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Ponto
+{
+    public: 
+    float x,y;
+
+};
+class Poligono 
+{
+    public:
+    vector<Ponto> pontos;
+};
+
